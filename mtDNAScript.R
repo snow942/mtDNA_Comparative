2013c7e7e2b37126cdda81f79e361e26ce35cb61
Ryan Snow <snow4964@gmail.com>
2023-03-13 16:03:47 -0400

    Add files via upload
    Changes to mtDNAScript.R. Implemented a means to filter data from .snps file according to variant depth presence. As well as incorporating that score in a vcf file for filter application

--- mtDNAScript.R
+++ mtDNAScript.R
@@ -3,23 +3,22 @@
 
 library (dplyr)
 library (devtools)
-#library (magrittr)
+library (magrittr)
 library (GenomicRanges)
-#library (knitr)
-#library (ggplot2)
+library (knitr)
+library (ggplot2)
 library (tidyr)
 library (rmarkdown)
-#library (rutilstimflutre)
-#library (ggbio)
-#library (IRanges)
+library (tibble)
+
 
 #Multiple sequence alignment completed via Nucmer, using the MUMmer package/software
 
 #Read file(s)
-Sfa_ABas.snps <- read.table (file="/Users/snow4/Desktop/MUMmer/Sfa_ABas_Query.fasta.snps")
-Sfa_CBas.snps <- read.table (file="/Users/snow4/Desktop/MUMmer/Sfa_CBas_Query.fasta.snps")
+Sfa_ABas.snps <- read.table (file="/Users/snow4/Desktop/Mummerv4/SNPS/Sfa_ABas_Query.fasta.snps")
+Sfa_CBas.snps <- read.table (file="/Users/snow4/Desktop/Mummerv4/SNPS/Sfa_CBas_Query.fasta.snps")
 
-Sfa_Bas.snps <- read.table (file="/Users/snow4/Desktop/MUMmer/Sfa_Bas_Query.fasta.snps")
+Sfa_Bas.snps <- read.table (file="/Users/snow4/Desktop/Mummerv4/SNPS/Sfa_Bas_Query.fasta.snps")
 
 #Change column names to better represent data/incorporate metadata
 #https://mummer.sourceforge.net/manual/#snps review output format for more info
@@ -33,8 +32,8 @@ colnames (Sfa_ABas.snps) <- c ("P1",
                                "Q",
                                "LEN_R",
                                "LEN_Q",
-                               "FRM_1",
-                               "FRM_2",
+                               "FRM1",
+                               "FRM2",
                                "mtDNA_Rep",
                                "SampleName")
 
@@ -48,30 +47,29 @@ colnames (Sfa_CBas.snps) <- c ("P1",
                                "Q",
                                "LEN_R",
                                "LEN_Q",
-                               "FRM_1",
-                               "FRM_2",
+                               "FRM1",
+                               "FRM2",
                                "mtDNA_Rep",
                                "SampleName")
 
 colnames (Sfa_Bas.snps) <- c ("P1",
-                               "SUB_Ref",
-                               "SUB_Query",
-                               "P2",
-                               "BUFF",
-                               "DIST",
-                               "R",
-                               "Q",
-                               "LEN_R",
-                               "LEN_Q",
-                               "FRM_1",
-                               "FRM_2",
-                               "mtDNA_Rep",
-                               "SampleName")
-
+                              "SUB_Ref",
+                              "SUB_Query",
+                              "P2",
+                              "BUFF",
+                              "DIST",
+                              "R",
+                              "Q",
+                              "LEN_R",
+                              "LEN_Q",
+                              "FRM1",
+                              "FRM2",
+                              "mtDNA_Rep",
+                              "SampleName")
 
 HighConfidenceVariants <- function (SnpsInput) {
 #Used to call high confidence (90%) variants in .snps files
-  
+  ###SnpsInput <- Sfa_ABas.snps
   #Determines number of total samples to implement a filter based on 90% of sample population
   SampleCounts.df <- data.frame (table (SnpsInput$SampleName))
   TotalSamples <- nrow (SampleCounts.df)
@@ -79,14 +77,17 @@ HighConfidenceVariants <- function (SnpsInput) {
   Filter <- TotalSamples*0.90
   
   #Counts the number of times a position appears in the dataset
+  Positions.df <- data.frame (Var1=SnpsInput$P1, Ref=SnpsInput$SUB_Ref, Query=SnpsInput$SUB_Query)
   Counts.df <- data.frame (table (SnpsInput$P1))
+  Positions.df$Var1 <- as.factor (Positions.df$Var1)
   
-  #Filters Counts.df to show only variant positions in 90% of sample population
-  Pos.filtered <- as_tibble (
-    Counts.df$Var1[(which (
-      Counts.df$Freq >= Filter))])
-  
-  return (Pos.filtered)
+  To_Filter <- left_join (Counts.df, Positions.df, by=c("Var1"), multiple="first")
+  colnames(To_Filter)[colnames(To_Filter) == "Var1"] ="POS"
+  #Filters To_Filter to show only variant positions in 90% of sample population
+  To_Filter.df <- data.frame (To_Filter)
+  Output <- To_Filter.df %>% filter (Freq >= Filter)
+    
+  return (Output)
   
   }
 
@@ -101,45 +102,65 @@ print (Sfa_CBas.Filtered)
 ABas <- data.frame (Sfa_ABas.Filtered)
 CBas <- data.frame (Sfa_CBas.Filtered)
 
-Unique.ABas <- data.frame (setdiff (ABas$value, CBas$value))
-Unique.CBas <- data.frame (setdiff (CBas$value, ABas$value))
-
-
-
-
-
+#Compares ABas and CBas df to identify SNP positions unique to the population
+Unique.ABas <- as_tibble (setdiff (ABas$Var1, CBas$Var1))
+#Unique.CBas <- data.frame (setdiff (CBas$value, ABas$value))
 
             
 ####
 
-#Trying to create a circle plot that shows the location of each called var in the mitogenome
 
-#https://taylorreiter.github.io/2019-05-11-Visualizing-NUCmer-Output/
+#FakeDelta <- function (Filtered, Snps) {
 
-FastaIndex <- read.table ("/Users/snow4/Desktop/mtDNARef/Sfa_mtDNA_Ref.fasta.fai", 
-                          header = FALSE, stringsAsFactors = FALSE,
-                          col.names = c ("Name", "Contig.Len", "Offset",
-                                         "LineBases", "LineWidth"))
+#  Snps$P1 <- as.factor (Snps$P1)
+#  Fake <- left_join (Filtered, Snps, by=c("Var1" = "P1"), multiple="first")
 
-GRangeRef <- GRanges (seqnames = Rle (FastaIndex$Name),
-                      ranges = IRanges (start = rep (1, nrow (FastaIndex)),
-                                        end = FastaIndex$Contig.Len))
+#  return (Fake)
+  
+#}
 
-seqlengths (GRangeRef) <- FastaIndex$Contig.Len
-genome (GRangeRef) <- "Reference"
-GRangeRef <- sortSeqlevels (GRangeRef)
-GRangeRef <- sort (GRangeRef)
+#ABas.Filtered.delta <- FakeDelta (ABas, Sfa_ABas.snps)
+#CBas.Filtered.delta <- FakeDelta (CBas, Sfa_CBas.snps)
+
+####
+
+library ("vcfR")
+
+ABas.vcf <- read.vcfR ("/Users/snow4/Desktop/Mummerv4/Sfa_ABas_Query.fasta.vcf")
+CBas.vcf <- read.vcfR ("/Users/snow4/Desktop/Mummerv4/Sfa_CBas_Query.fasta.vcf")
+
+ReadDepth <- function (vcf, Filtered.df) {
+#Incorporate freq as QUAL scores for filtering of vcf
+  
+  vcf.df <- vcfR2tidy (vcf)
+  Filtered.df$POS <- as.integer( as.character(Filtered.df$POS))
+  #Changes class of df values to integer, where available
+  
+  merge.vcf <- full_join (vcf.df$fix, Filtered.df, by= "POS")
+  #Merges the data from the vcf file and dataframe 
+    #Uses "POS" column as like values
+  merge.vcf <- merge.vcf %>% relocate (Freq, .after = "QUAL")
+  #Moves Freq column position to after QUAL
+  merge.vcf <- (merge.vcf[,!names(merge.vcf) %in%
+                            c ("QUAL", "Ref", "Query", "ChromKey")])
+  #Removes column with no signif value to exp
+  colnames(merge.vcf)[colnames(merge.vcf) == "Freq"] ="QUAL"
+  #Rename Freq column to QUAL
+  merge.vcf$QUAL[is.na(merge.vcf$QUAL)] <- 0
+  #All NAs in QUAL are changes to 0s
+  INFO = c (NA)
+  merge.vcf <- cbind(merge.vcf, INFO)
+  #Appends INFO column that was lost due to unknown reasons
+  vcf@fix <- as.matrix(merge.vcf)
+  #Replaces old QUAL scores with new ones by replacing "fix" matrix in vcf
+  
+  return (vcf)
+  }
+
+ABas.DP.vcf <- ReadDepth (ABas.vcf, ABas)
+CBas.DP.vcf <- ReadDepth (CBas.vcf, CBas)
 
 
-
-CirclePlot <- ggbio () +
-  circle (ABas, geom = "rect",
-         aes (color = "red", fill = "red")) +
-  circle (GRangeRef, geom = "ideo",
-          aes (color = "gray", fill = "gray")) +
-  # circle (GRangeRef, geom = "scale",
-  #        scale.type = "sci", size = 1.5) +
-  circle(ABas$value, geom = "text",
-         aes(label = seqnames), size = 1) 
-
-CirclePlot
+write.vcf (ABas.DP.vcf, file = "ABas.Filter.vcf.gz", mask = FALSE, APPEND = FALSE)
+write.vcf (CBas.DP.vcf, file = "CBas.Filter.vcf.gz", mask = FALSE, APPEND = FALSE)
+#Files need to be written as zipped files
